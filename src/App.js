b90a9b7115f8aa20812140f9e84928766d6ef1d8
hblaxton <122068882+hblaxton@users.noreply.github.com>
2023-02-07 14:26:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+<>
+    
+
+  <div class="buttons">
+  <button class="duplicate">Duplicate last card</button>
+  <button class="modifytitle">Change Name</button>
+  <button id="bg" class="button">Change Background</button>
+  <button id="deletelastcard">Delete</button>
+  <button data-toggle-btn>Toggle description</button>
+</div>
+<div class="wrapper">
+  <div class="container">
+  <img class="image" src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg"/>
+  <div class="header">
+    <h3>Landscaping Company</h3>
+    <h4>A beautiful green tree</h4>
+      </div>
+      <details class="details">
+      <summary>Description</summary>
+      <div>
+      <ul>
+      <li>A tree as beautiful as this one should be cut down</li>
+        <li>Trees!</li>
+       </ul>
+    </div>
+    </details>
+  </div>
+  
+</div>
+
+export default App; </>);
+};
